fecd0303c00944aa5df89df6cb1bd1e05c32b850
BrunoSerpa <serpabruno2005@gmail.com>
2024-11-09 13:16:05 -0300

    Arrumei a conexão com o banco

--- Programa/conexaoBanco/conectar.py
+++ Programa/conexaoBanco/conectar.py
@@ -1,29 +1,12 @@
-from cassandra.cluster import Cluster
-from cassandra.auth import PlainTextAuthProvider
+from neo4j import GraphDatabase
 
-from Programa.conexaoBanco.configuracao import configuracao, tabelas, indices
+from Programa.conexaoBanco.credenciais import uri, user, password
 from Programa.funcoes.utils.salvarErro import salvarErro
 
 def conectar():
     try:
-        configNuvem, idCliente, segredoCliente = configuracao()
-        provedorAutenticacao = PlainTextAuthProvider(idCliente, segredoCliente)
-        cluster = Cluster(cloud=configNuvem, auth_provider=provedorAutenticacao)
-        sessao = cluster.connect()
-        sessao.execute("USE loja_online;")
-        # for query in tabelas():
-        #     try:
-        #         sessao.execute(query)
-        #         print(f"Comando executado com sucesso: {query}")
-        #     except Exception as e:
-        #         salvarErro("Erro ao criar tabela", e)
-        # for query in indices():
-        #     try:
-        #         sessao.execute(query)
-        #         print(f"Comando executado com sucesso: {query}")
-        #     except Exception as e:
-        #         salvarErro("Erro ao criar índice", e)
-        return sessao
+        driver = GraphDatabase.driver(uri, auth = (user, password))
+        return driver.session()
     except Exception as e:
         salvarErro("Erro ao conectar ao Cassandra", e)
         return None
