83d56544db404542a096085fe9041bd984b16de0
BrunoSerpa <serpabruno2005@gmail.com>
2024-11-10 23:47:36 -0300

    Arrumei o crud

--- Programa/funcoes/crud.py
+++ Programa/funcoes/crud.py
@@ -1,128 +1,151 @@
 from Programa.conexaoBanco.conectar import conectar
 from Programa.funcoes.utils.salvarErro import salvarErro
-from Programa.funcoes.utils.converter import rowParaDict
 
-from cassandra.query import SimpleStatement
-from uuid import uuid4, UUID
+from uuid import uuid4
 
-sessao = conectar()
+driver, sessao = conectar()
 
-def escolherColecao(nome):
+def escolherLabel(nome):
     if sessao is None:
         salvarErro("Erro na sessão", "Sessão de banco de dados não estabelecida")
         return None
 
-    colecoes = {
-        'Usuarios': 'usuarios',
-        'Vendedores': 'vendedores',
-        'Produtos': 'produtos',
-        'Compras': 'compras',
-        'Enderecos': 'enderecos'
+    labels = {
+        'Usuarios': 'Usuario',
+        'Vendedores': 'Vendedor',
+        'Produtos': 'Produto',
+        'Compras': 'Compra',
+        'Enderecos': 'Endereco'
     }
-    return colecoes.get(nome)
+    return labels.get(nome)
 
-def cadastrar(nomeColecao, dados):
-    colecao = escolherColecao(nomeColecao)
-    if colecao is None:
+def cadastrar(nomeLabel, dados):
+    label = escolherLabel(nomeLabel)
+    if label is None:
         return None
     try:
-        colunas = ', '.join(dados.keys())
-        valores = []
-        
-        for v in dados.values():
-            if isinstance(v, UUID) or isinstance(v, float) or isinstance(v, int):
-                valores.append(f"{v}")
-            elif isinstance(v, set):
-                set_values = ', '.join([str(uuid) for uuid in v])
-                valores.append(f"{{{set_values}}}")
-            else:
-                valores.append(f"'{v}'")
-        
-        valores_str = ', '.join(valores)
-        id = uuid4()
-        query = f"INSERT INTO {colecao} (id, {colunas}) VALUES ({id}, {valores_str})"
-        sessao.execute(SimpleStatement(query))
+        id = str(uuid4())
+        dados['id'] = id
+
+        propriedades = ', '.join(f"{k}: ${k}" for k in dados.keys())
+
+        query = f"CREATE (n:{label} {{{propriedades}}})"
+        sessao.run(query, dados)
+        for key, value in dados.items():
+            if isinstance(value, str) and key in ["id_vendedor", "remetente", "destinatario", "id_cliente"]:
+                vincularRelacao(id, key, value, label)
         return id
 
     except Exception as e:
-        salvarErro(f"Erro ao cadastrar em {nomeColecao}", e)
+        salvarErro(f"Erro ao cadastrar em {nomeLabel}", e)
         return None
 
-def atualizar(nomeColecao, dados):
-    colecao = escolherColecao(nomeColecao)
-    if colecao == None:
+def atualizar(nomeLabel, dados):
+    label = escolherLabel(nomeLabel)
+    if label == None:
         return None
     try:
-        updates = []
-        for k, v in dados.items():
-            if k != "id":
-                if isinstance(v, set):
-                    set_values = ', '.join([str(uuid) for uuid in v])
-                    updates.append(f"{k} = {{{set_values}}}")
-                elif isinstance(v, (int, float)):
-                    updates.append(f"{k} = {v}")
-                elif isinstance(v, UUID):
-                    updates.append(f"{k} = {str(v)}")
-                else:
-                    updates.append(f"{k} = '{v}'")
-                    
-        updates_str = ', '.join(updates)
-        query = f"UPDATE {colecao} SET {updates_str} WHERE id = {dados['id']}"
-        sessao.execute(SimpleStatement(query))
-        return dados["id"]
+        id = dados.pop('id', None)
+        if not id:
+            salvarErro(f"Erro ao atualizar {nomeLabel}", "ID não fornecido")
+            return None
+
+        updates = ', '.join(f"n.{k} = ${k}" for k in dados.keys())
+        query = f"MATCH (n:{label} {{id: '{id}'}}) SET {updates}"
+        sessao.run(query, dados)
+
+        for key, value in dados.items():
+            if isinstance(value, list):
+                vincularRelacao(id, key, value, label)
+        dados['id'] = id
+        return id
+
     except Exception as e:
-        salvarErro(f"Erro ao atualizar {nomeColecao}", e)
+        salvarErro(f"Erro ao atualizar {nomeLabel}", e)
         return None
 
-def excluir(nomeColecao, id):
-    colecao = escolherColecao(nomeColecao)
-    if colecao == None:
+
+def excluir(nomeLabel, id):
+    label = escolherLabel(nomeLabel)
+    if label == None:
         return None
     try:
-        query = f"DELETE FROM {colecao} WHERE id = {id}"
-        sessao.execute(SimpleStatement(query))
+        query = f"MATCH (n:{label} {{id: $id}})-[r]-() DELETE r"
+        sessao.run(query, {"id": id})
+    except Exception as e:
+        salvarErro(f"Erro ao excluir relacionamentos {nomeLabel}", e)
+        return None
+    try:
+        query = f"MATCH (n:{label} {{id: $id}}) DELETE n"
+        sessao.run(query, {"id": id})
         return id
     except Exception as e:
-        salvarErro(f"Erro ao excluir em {nomeColecao}", e)
+        salvarErro(f"Erro ao excluir em {nomeLabel}", e)
         return None
 
-def buscarTodos(nomeColecao):
-    colecao = escolherColecao(nomeColecao)
-    if colecao == None:
+def buscarTodos(nomeLabel):
+    label = escolherLabel(nomeLabel)
+    if label == None:
         return None
     try:
-        query = f"SELECT * FROM {colecao}"
-        resultados = sessao.execute(SimpleStatement(query))
-        return [rowParaDict(row, colecao) for row in resultados]
+        query = f"MATCH (n:{label}) RETURN n"
+        resultados = sessao.run(query)
+        return [dict(record['n']) for record in resultados]
     except Exception as e:
         salvarErro("Erro ao buscar todos os registros", e)
     return None
 
-def buscarPorId(nomeColecao, id):
-    colecao = escolherColecao(nomeColecao)
-    if colecao == None:
+def buscarPorId(nomeLabel, id):
+    label = escolherLabel(nomeLabel)
+    if label == None:
         return None
     try:
-        query = f"SELECT * FROM {colecao} WHERE id = {id}"
-        prepared_stmt = SimpleStatement(query)
-        resultado = sessao.execute(prepared_stmt).one()
+        query = f"MATCH (n:{label} {{id: $id}}) RETURN n"
+        resultado = sessao.run(query, {"id": id}).single()
         if resultado:
-            return rowParaDict(resultado, colecao)
+            return dict(resultado['n'])
         else:
-            salvarErro("Registro não encontrado", f'ID {id} não encontrado na colecao {nomeColecao}')
+            salvarErro("Registro não encontrado", f'ID {id} não encontrado na label {nomeLabel}')
             return None
     except Exception as e:
         salvarErro("Erro ao buscar registro por ID", e)
         return None
 
-def buscarPorAtributo(nomeColecao, nomeCampo, atributo):
-    colecao = escolherColecao(nomeColecao)
-    if colecao == None:
+def buscarPorAtributo(nomeLabel, nomeCampo, atributo):
+    label = escolherLabel(nomeLabel)
+    if label == None:
         return None
     try:
-        query = f"SELECT * FROM {colecao} WHERE {nomeCampo} = '{atributo}'"
-        resultados = sessao.execute(SimpleStatement(query))
-        return [rowParaDict(row, colecao) for row in resultados]
+        query = f"MATCH (n:{label}) WHERE n.{nomeCampo} = $atributo RETURN n"
+        resultados = sessao.run(query, {'atributo': atributo})
+        return [dict(record['n']) for record in resultados]
     except Exception as e:
         salvarErro("Erro ao buscar registro por atributo semelhante", e)
         return None
+
+def vincularRelacao(id, campo, item_id, label):
+    relacoes = {
+        "enderecos": "TEM_ENDERECO",
+        "favoritos": "FAVORITOU",
+        "compras": "FEZ_COMPRA",
+        "vendas": "REALIZA_VENDA",
+        "produtos": "TEM_PRODUTO",
+        "id_vendedor": "VENDIDO_POR",
+        "remetente": "ENVIADO_POR",
+        "destinatario": "ENVIADO_A",
+        "id_cliente": "COMPRADO_POR"
+    }
+
+    if campo in relacoes:
+        relacao = relacoes[campo]
+        try:
+            if isinstance(item_id, list):
+                for id_item in item_id:
+                    query = f"MATCH (u:{label} {{id: $id}}), (e {{id: $item_id}}) MERGE (u)-[:{relacao}]->(e)"
+                    sessao.run(query, {"id": id, "item_id": id_item})
+            else:
+                query = f"MATCH (u:{label} {{id: $id}}), (e {{id: $item_id}}) MERGE (u)-[:{relacao}]->(e)"
+                sessao.run(query, {"id": id, "item_id": item_id})
+        except Exception as e:
+            salvarErro(f"Erro ao criar a relação {relacao}", e)
+            return False
